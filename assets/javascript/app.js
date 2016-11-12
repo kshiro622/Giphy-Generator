3ce47f0e5c90c3f4b96ed33868598d131d91cb53
Katherine Shiro <shirok@dhcp-10-102-173-252.wireless.northwestern.private>
2016-11-12 09:38:10 -0600

    https

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     $(document).on('click', '.dad-button', function () {
         $('#dads').empty();
         var dadName = $(this).data('name');
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + dadName + "&api_key=dc6zaTOxFJmzC&limit=10";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + dadName + "&api_key=dc6zaTOxFJmzC&limit=10";
         $.ajax({
             url: queryURL,
             method: 'GET'
